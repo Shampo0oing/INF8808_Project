fc72eeae398b9f641ef532958f677c5935cbc4e8
Junnie <ngocdung2805@gmail.com>
2024-04-07 20:38:57 -0400

    Add modal for bar chart by year

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -4,28 +4,36 @@
     <button class="tab" :class="{ active: activeTab === 'all' }" @click="changeTab('all')">Vue générale</button>
     <button class="tab" :class="{ active: activeTab === 'byYear' }" @click="changeTab('byYear')">Vue par année</button>
   </div>
+  <div v-if="showModal" class="modal-backdrop" @click="closeModal"></div>
   <div id="chartAll" class="chart"></div>
   <div id="chartByYear" class="chart" style="display: none;">
     <div class="chart-row">
-      <div id="chart2011"></div>
-      <div id="chart2012"></div>
-      <div id="chart2013"></div>
+      <div id="chart2011" @click="showChart('chart2011')"></div>
+      <div id="chart2012" @click="showChart('chart2012')"></div>
+      <div id="chart2013" @click="showChart('chart2013')"></div>
+      <div id="chart2014" @click="showChart('chart2014')"></div>
     </div>
     <div class="chart-row">
-      <div id="chart2014"></div>
-      <div id="chart2015"></div>
-      <div id="chart2016"></div>
+      <div id="chart2015" @click="showChart('chart2015')"></div>
+      <div id="chart2016" @click="showChart('chart2016')"></div>
+      <div id="chart2017" @click="showChart('chart2017')"></div>
+      <div id="chart2018" @click="showChart('chart2018')"></div>
     </div>
     <div class="chart-row">
-      <div id="chart2017"></div>
-      <div id="chart2018"></div>
-      <div id="chart2019"></div>
+      <div id="chart2019" @click="showChart('chart2019')"></div>
+      <div id="chart2020" @click="showChart('chart2020')"></div>
+      <div id="chart2021" @click="showChart('chart2021')"></div>
+      <div id="chart2022" @click="showChart('chart2022')"></div>
     </div>
-    <div class="chart-row">
-      <div id="chart2020"></div>
-      <div id="chart2021"></div>
-      <div id="chart2022"></div>
-    </div>
+  </div>
+  <div id="chartModal" v-if="showModal" class="modal">
+    <button @click="closeModal" class="close-button">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+      </svg>
+    </button>
+    <div id="chartModalContent" class="modal-content"></div>
   </div>
 </template>
 
@@ -36,8 +44,10 @@ export default {
   data() {
     return {
       activeTab: 'all',
+      showModal: false,
       accidentCounts: null,
-      years: Array.from({ length: 12 }, (_, i) => i + 2011)
+      years: Array.from({ length: 12 }, (_, i) => i + 2011),
+      yearsData: {}
     };
   },
   async mounted() {
@@ -65,12 +75,24 @@ export default {
       if (tab === 'all') {
         document.getElementById('chartAll').style.display = 'block';
         document.getElementById('chartByYear').style.display = 'none';
+        this.$nextTick(() => {
+          const chartAll = document.getElementById('chartAll');
+          chartAll.style.display = 'flex';
+          chartAll.style.justifyContent = 'center';
+          chartAll.style.alignItems = 'center';
+          chartAll.style.height = '100vh';
+          chartAll.style.paddingBottom = '80px';
+        });
       } else if (tab === 'byYear') {
         document.getElementById('chartAll').style.display = 'none';
         document.getElementById('chartByYear').style.display = 'block';
       }
     },
     getDataForYear(year) {
+      //check if data for the year is available from yearsData
+      if (this.yearsData[year]) {
+        return this.yearsData[year];
+      }
       let data;
       if (year === 'all') {
         data = Object.values(this.accidentCounts).reduce((acc, yearData) => {
@@ -83,6 +105,8 @@ export default {
         data = this.accidentCounts[year];
       }
       data = this.processAccidentLabel(data);
+      //store the data for the year in yearsData
+      this.yearsData[year] = data;
       return data;
     },
     processAccidentLabel(data) {
@@ -118,11 +142,11 @@ export default {
       // Create bar chart for each year
       this.years.forEach(year => {
         const dataForYear = this.getDataForYear(year);
-        this.createBarChartForData(dataForYear, '#chart' + year, 400, 300);
+        this.createBarChartForData(dataForYear, '#chart' + year, year, 400, 300, true);
       });
     },
-    createBarChartForData(data, chartRef, maxwidth = 800, maxheight = 600) {
-      const margin = {top: 20, right: 20, bottom: 50, left: 100},
+    createBarChartForData(data, chartRef, year = '', maxwidth = 800, maxheight = 600, textanchor = false) {
+      const margin = {top: 60, right: 20, bottom: 50, left: 100},
       width = maxwidth - margin.left - margin.right,
       height = maxheight - margin.top - margin.bottom;
 
@@ -150,7 +174,9 @@ export default {
           .attr('y', d => y(d.count))
           .attr('width', x.bandwidth())
           .attr('height', d => height - y(d.count))
-          .attr('fill', 'steelblue');
+          .attr('fill', 'steelblue')
+          .append('title')
+          .text(d => `Type: ${d.type}\nNombre d'accidents: ${d.count}`);
 
       svg.selectAll('.label')
           .data(data)
@@ -169,7 +195,7 @@ export default {
       const xAxis = svg.append("g")
           .attr("transform", "translate(0," + height + ")")
           .call(d3.axisBottom(x));
-      if (chartRef === '#chartAll') {
+      if (textanchor === false) {
         xAxis.selectAll(".tick text")
           .call(this.wrap, x.bandwidth());
       } else {
@@ -177,6 +203,16 @@ export default {
           .attr('transform', 'rotate(-45)')
           .style('text-anchor', 'end')
           .call(this.wrap, x.bandwidth());
+      }
+
+      if (year !== '') {
+        svg.append('text')
+          .attr('x', width / 2)
+          .attr('y', 0 - (margin.top / 2))
+          .attr('text-anchor', 'middle')
+          .style('font-size', '16px')
+          .style('text-decoration', 'underline')
+          .text(year); 
       }
 
       // Add y-axis
@@ -205,12 +241,32 @@ export default {
           }
         }
       });
+    },
+    showChart(chartId) {
+      this.showModal = true;
+      this.$nextTick(() => {
+        const year = chartId.replace('chart', '');
+        const dataForChart = this.getDataForYear(year);
+        d3.select('#chartModalContent').selectAll('*').remove();
+        this.createBarChartForData(dataForChart, '#chartModalContent', year, 800, 600);
+      });
+    },
+    closeModal() {
+      this.showModal = false;
     }
   }
 };
 </script>
 
 <style scoped>
+#chartAll {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding-bottom: 80px;
+}
+
 .tab {
   display: inline-block;
   padding: 10px 20px;
@@ -235,5 +291,37 @@ export default {
 .chart-row {
   display: flex;
   justify-content: space-between;
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000;
+  background-color: rgb(255, 255, 255);
+}
+
+.modal-content {
+  padding-bottom: 20px;
+}
+
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  cursor: pointer;
 }
 </style>
