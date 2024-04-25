e04a3cdef5a1cb0c2dee88c8cf50ca095da3ff7f
Nosaj <jason.wong@polymtl.ca>
2024-04-25 15:19:25 -0400

    removed comments

--- src/components/Sankey.vue
+++ src/components/Sankey.vue
@@ -1,14 +1,10 @@
 <script setup>
 import * as d3 from 'd3'
 import { sankey, sankeyLinkHorizontal } from 'd3-sankey'
+import useColorPalette from '~/composables/color'
 import chartData from '~/data/sankey/Sankey.json'
 
-/* const props = defineProps({
-  accidents: {
-    type: Array,
-    required: true,
-  },
-}); */
+const color = useColorPalette()
 
 const sankeyContainer = ref(null)
 
@@ -33,112 +29,6 @@ function initialize() {
         [50, 150],
         [900, 800],
       ])
-    /*
-    const allData = props.accidents.flat();
-    // Define the names of columns to check for emptiness
-    const columnsToCheck = ["CD_ECLRM", "GRAVITE", "CD_ENVRN_ACCDN"];
-
-    // Filter the data
-    const filteredAccidents = allData.filter((accident) => {
-      // Check if all specified columns have non-empty values
-      return columnsToCheck.every((columnName) => {
-        // Check if the value of the column is not empty
-        return accident[columnName] !== "";
-      });
-    });
-
-    // Filter accidents into day and night categories
-    const dayAccident = filteredAccidents.filter((accident) => {
-      return accident.CD_ECLRM === "1" || accident.CD_ECLRM === "2";
-    });
-    const nightAccident = filteredAccidents.filter((accident) => {
-      return accident.CD_ECLRM === "3" || accident.CD_ECLRM === "4";
-    });
-
-    const accidentCounts = new Map();
-    filteredAccidents.forEach((accident) => {
-      // Determine if the accident is a day or night accident
-      const isDayAccident =
-        accident.CD_ECLRM === "1" || accident.CD_ECLRM === "2";
-      const timeOfDay = isDayAccident ? "Jour" : "Nuit";
-
-      // Key for time of the day and severity
-      const keyDaySeverity = `${timeOfDay}-${accident.GRAVITE}`;
-
-      // Mapping for CD_ENVRN_ACCDN to words
-      const environmentMapping = {
-        1: "Scolaire",
-        2: "Résidentiel",
-        3: "Commercial",
-        4: "Industriel",
-        5: "Rural",
-        6: "Autre",
-        9: "Autre",
-      };
-
-      // Key for severity and environment
-      const keySeverityEnvironment = `${accident.GRAVITE}-${environmentMapping[accident.CD_ENVRN_ACCDN]}`;
-
-      // Update counts for day/severity and severity/environment
-      if (accidentCounts.has(keyDaySeverity)) {
-        accidentCounts.set(
-          keyDaySeverity,
-          accidentCounts.get(keyDaySeverity) + 1
-        );
-      } else {
-        accidentCounts.set(keyDaySeverity, 1);
-      }
-      if (accidentCounts.has(keySeverityEnvironment)) {
-        accidentCounts.set(
-          keySeverityEnvironment,
-          accidentCounts.get(keySeverityEnvironment) + 1
-        );
-      } else {
-        accidentCounts.set(keySeverityEnvironment, 1);
-      }
-    });
-
-    // Dynamically generate links based on the counts
-    const links = [];
-
-    // Manually push first 2 links for day and night accidents
-    links.push({
-      source: "Accident",
-      target: "Jour",
-      value: dayAccident.length,
-    });
-    links.push({
-      source: "Accident",
-      target: "Nuit",
-      value: nightAccident.length,
-    });
-
-    accidentCounts.forEach((value, key) => {
-      const [part1, part2] = key.split("-");
-      let source, target;
-
-      // Determine source and target based on the key
-      if (part1 === "1" || part1 === "2") {
-        // This is a day/severity key
-        source = part1 === "1" ? "Jour" : "Nuit";
-        target = part2; // Severity
-      } else {
-        // This is a severity/environment key
-        source = part1; // Severity
-        target = part2; // Environment
-      }
-
-      // Generate the link
-      if (source && target) links.push({ source, target, value });
-    });
-
-    // Create the nodes array based on unique names from the links
-    const nodeNames = new Set();
-    links.forEach((link) => {
-      nodeNames.add(link.source);
-      nodeNames.add(link.target);
-    });
-    const nodes = Array.from(nodeNames).map((name) => ({ name })); */
 
     // Create the data object from json file
     const data = chartData
@@ -146,7 +36,7 @@ function initialize() {
     sankeyLayout(data)
 
     // Draw the nodes
-    const node = svg
+    svg
       .append('g')
       .selectAll('rect')
       .data(data.nodes)
@@ -155,16 +45,7 @@ function initialize() {
       .attr('y', d => d.y0)
       .attr('height', d => d.y1 - d.y0)
       .attr('width', d => d.x1 - d.x0)
-      .attr('fill', 'lightblue')
-
-    // Add hover effect to nodes
-    node
-      .on('mouseover', function () {
-        d3.select(this).attr('fill', 'orange')
-      })
-      .on('mouseout', function () {
-        d3.select(this).attr('fill', 'lightblue')
-      })
+      .attr('fill', color.blue)
 
     // Add labels to the nodes
     svg
@@ -205,7 +86,7 @@ function initialize() {
       .attr('stroke', 'black')
       .attr('stroke-width', d => Math.max(2, d.width)) // Set the width of the link based on the value
       .on('mouseover', (event, d) => {
-        d3.select(event.target).attr('stroke', 'red')
+        d3.select(event.target).attr('stroke', color.green)
         tooltip
           .style('opacity', 1)
           .attr('x', (d.source.x1 + d.target.x0) / 2)
