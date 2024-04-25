8002ac37381e5110497a570409a90e69860d24d2
Anthelme <thoter871@gmail.com>
2024-04-25 18:53:10 -0400

    useHead

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<script setup>
+useHead({
+  title: 'Vitesse',
+  meta: [
+    {
+      name: 'description',
+      content: 'Opinionated Vite Starter Template',
+    },
+    {
+      name: 'theme-color',
+      content: () => isDark.value ? '#00aba9' : '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
+    },
+  ],
+})
+</script>
+
 <template>
   <main font-sans text="center gray-700 dark:gray-200">
     <RouterView />
